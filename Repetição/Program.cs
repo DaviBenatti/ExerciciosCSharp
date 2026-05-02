6ee7d1561fcb568114f6ca727502d6d0cb86e9d5
DaviBenatti <davilopesbenatti@gmail.com>
2026-05-02 15:34:36 -0300

    :sparkles: feat: Exercícios de loop finalizados

--- Repetição/Program.cs
+++ Repetição/Program.cs
@@ -176,21 +176,113 @@ class Program
 
         // 4) MENU COM REPETIÇÃO(DO WHILE)
 
-        int opcao;
+        // int opcao = 0;
 
-        do
-        {
-            Console.WriteLine("1 -> Mostrar mensagem");
-            Console.WriteLine("2 -> Sair");
-            Console.Write("Selecione uma opção: ");
-            opcao = int.Parse(Console.ReadLine());
+        // do
+        // {
+        //     Console.WriteLine("1 -> Mostrar mensagem");
+        //     Console.WriteLine("2 -> Sair");
+        //     Console.Write("Selecione uma opção: ");
+        //     opcao = int.Parse(Console.ReadLine());
             
-            if(opcao < 1 || opcao > 2)
-            {
-                Console.WriteLine("Digite uma opção válida!");
-            }
-        } while(opcao != 2);
+        //     if(opcao < 1 || opcao > 2)
+        //     {
+        //         Console.WriteLine("Digite uma opção válida!");
+        //     }
+        // } while(opcao != 2);
         
-        Console.WriteLine("Até a próxima!");
+        // Console.WriteLine("Até a próxima!");
+
+        // 5) MÉDIA DE VALORES (FOR)
+
+        // double soma = 0;
+        // int n = 0;
+
+        // for(int i = 1; i <= 4; i++)
+        // {
+        //     Console.Write("Digite o " + i + "º número: ");
+        //     double num = double.Parse(Console.ReadLine());
+        //     soma += num;
+        //     n += 1;
+        // }
+
+        // double media = soma / n;
+
+        // Console.WriteLine("A média dos valores é: " + media);
+
+        // NÍVEL DIFÍCIL
+
+        // 1) MAIOR NÚMERO DIGITADO (FOR)
+
+        // int maior = 0;
+
+        // for(int i = 1; i <= 5; i++)
+        // {
+        //     Console.Write("Digite o " + i + "º número: ");
+        //     int num = int.Parse(Console.ReadLine());
+
+        //     if(num > maior)
+        //     {
+        //         maior = num;
+        //     }
+        // }
+
+        // Console.WriteLine("O maior número digitado é: " + maior);
+
+        // 2) CONTAGEM COM CONDIÇÃO(WHILE)
+
+        // Console.Write("Digite um número que seja maior que zero: ");
+        // int num = int.Parse(Console.ReadLine());
+        // int contador = 0;
+        
+        // while(num != 0)
+        // {
+        //     Console.Write("Digite um número que seja maior que zero: ");
+        //     num = int.Parse(Console.ReadLine());
+        //     contador += 1;
+        // }
+
+        // Console.WriteLine("Total de números digitados: " + contador);
+
+        // 3) SOMA ATÉ NÚMERO NEGATIVO(WHILE)
+
+        // Console.Write("Digite um número positivo: ");
+        // int num = int.Parse(Console.ReadLine());
+
+        // int soma = 0;
+
+        // while(num >= 0)
+        // {
+        //     soma += num;
+        //     Console.Write("Digite um número positivo: ");
+        //     num = int.Parse(Console.ReadLine());
+        // }
+
+        // Console.WriteLine("A soma dos números positivos: " + soma);
+
+        // 4) PERCORRENDO ARRAY(FOREACH)
+
+        // int[] numeros = {3, 7, 2, 9, 6};
+
+        // foreach(int numero in numeros)
+        // {
+        //     Console.WriteLine(numero);
+        // }
+
+        // 5) MÉDIA COM FOREACH
+
+        // double[] notas = {7.5, 8.5, 9.5, 4.0};
+        // int contador = 0;
+        // double soma = 0;
+
+        // foreach(double nota in notas)
+        // {
+        //     soma += nota;
+        //     contador += 1;
+        // }
+
+        // double media = soma / contador;
+
+        // Console.WriteLine("A média do aluno é: " + media);
     }
 }
